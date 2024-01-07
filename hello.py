cd526d3a3c8975adcd44237ecd23a3902c4321b5
김윤민 <dbsalszz@naver.com>
2024-01-07 18:28:54 +0900

    hello py 수정, pylint 통과

--- hello.py
+++ hello.py
@@ -12,14 +12,14 @@ def hello_world():
     hello world api
     """
     return "<p>Hello, WOrld! Is ci cd fun?</p>"
-  
+
 @app.route("/index")
 def index():
-  branch_condition = 1
-  if branch_condition == 1:
-    return "<p>index</p>"
     """
     index api
     """
-  return f"<p>index {branch_condition}</p>"
+    branch_condition = 1
+    if branch_condition == 1:
+        return "<p>index</p>"
+    return f"<p>index {branch_condition}</p>"
       
